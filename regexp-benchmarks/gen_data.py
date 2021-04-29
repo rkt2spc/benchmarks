d5c1b62b92706d36f5a6974db4503e77c429ff11
Tuan Nguyen <nmtuan.dev@gmail.com>
2021-04-29 12:08:31 +0800

    bump

--- regexp-benchmarks/gen_data.py
+++ regexp-benchmarks/gen_data.py
@@ -9,7 +9,7 @@ Edwards, who is now known as Jai Nitai Dasa, became a vegetarian, took up yoga, 
 Fifteen years later, he was working in investment banking in London. This was 1990s Britain, post–Margaret Thatcher, and the UK’s financial district was running with minimum government oversight and at the mercy of the free market. A lot of people were making a lot of money, fast.
 """
 
-    return [paragraph] * max(n, 0)
+    return [paragraph] * n
 
 def real_messages(n = None):
     messages = list(filter(None, json.load(open('messages.json'))))
